777dcd31efec3dc2e60c1ec8a4e92fa130f52e5f
rezaalemy <ralemy@curisprofound.com>
2018-10-05 05:51:39 -0700

    env variable in scripts

--- wildfly/config/wait-for-mysql.sh
+++ wildfly/config/wait-for-mysql.sh
@@ -11,4 +11,4 @@ done
 
 echo -e "\nmysql ready"
 export DISPLAY=:0.0
-/opt/jboss/wildfly/bin/standalone.sh -b 0.0.0.0 -bmanagement 0.0.0.0 -c standalone-full.xml
+${WILDFLY}/bin/standalone.sh -b 0.0.0.0 -bmanagement 0.0.0.0 -c standalone-full.xml
